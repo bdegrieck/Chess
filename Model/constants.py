8e592159a0d9c8cec0b72022d7419201431e6ea2
bdegrieck <bendegrieck@comcast.net>
2024-11-19 14:26:57 -0500

    comments

--- Model/constants.py
+++ Model/constants.py
@@ -2,6 +2,15 @@ from pydantic import BaseModel
 
 
 class ModelParams(BaseModel):
+    """
+    channel_values: a list of channel sizes
+    num_classes: number of output classification classes
+    num_layers: number of convolution layers
+    kernel_size: size of the kernel
+    stride: amount of steps the padding moves across the image
+    padding: adding extra layers of zeros around an image
+    x_size: size of the image
+    """
     channel_values: list[int]
     num_classes: int
     num_layers: int
